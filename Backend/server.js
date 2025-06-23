2cf291a79d494d95701a2c00f7413ed8abecc8da
G-RagulDharma <ragulsoorya27@gmail.com>
2025-06-23 15:51:28 +0530

    update:Env file

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://User:12345@demo.gar6xnx.mongodb.net/";
 mongoose
-  .connect(uri, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
